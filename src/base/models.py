4d96c47cb62090bd939c2a68838f18ad9c7e5cab
WolF2214 <manu.perea13@gmail.com>
2023-01-23 09:30:39 -0300

    upgrade

--- src/base/models.py
+++ src/base/models.py
@@ -3,18 +3,19 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Type(models.Model):
+    type_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.type_name.title()}'
+
 class Item(models.Model):
-    STATUS_CHOICES = (
-        ('A', 'Aridos'),
-        ('B', 'Mamposteria'),
-        ('C', 'Griferia')   
-    )
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    code = models.CharField('Codigo', max_length=30)
+    code = models.CharField('Codigo', max_length=30, null=True, blank=True)
     name = models.CharField('Nombre Item', max_length=30)
-    tipe = models.CharField('Tipo de Item', max_length=20, choices=STATUS_CHOICES, default='A')
-    price = models.CharField('Precio Unitario', max_length=30)
-    quantity = models.CharField('Cantidad en Stock',max_length=30)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True, blank=True)
+    price = models.IntegerField('Precio Unitario', default='0', null=True, blank=True)
+    quantity = models.IntegerField('Cantidad en Stock',default='0', null=True, blank=True )
 
     date_create = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
--- src/stockManager/settings.py
+++ src/stockManager/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
+# import environ
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+# env = environ.Env()
+# environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,11 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
-
+# DEBUG = env('DEBUG')
+SECRET_KEY = 'django-insecure-g2*tw1%ye%ckxju!*gb9sn6x$sraf*n$=3f0s6h+3k5xv7k143'
+DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
@@ -74,14 +75,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'stockManager.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': env('DATABASE_NAME'),
+#         'USER': env('DATABASE_USER'),
+#         'PASSWORD': env('DATABASE_PASSWORD'),
+#         'HOST': env('DATABASE_HOST'),
+#         'PORT': env('DATABASE_PORT')
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT')
+        'NAME': 'my-app-db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
